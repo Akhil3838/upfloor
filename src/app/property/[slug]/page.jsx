3b3142a58ccb542934791ce8b4bd51c2d07d29a1
akhil3838 <akhiltv713@gmail.com>
2025-10-03 17:09:10 +0530

    the

--- src/app/property/[slug]/page.jsx
+++ src/app/property/[slug]/page.jsx
@@ -1001,7 +1001,34 @@ console.log(property);
 <div className="col-xl-4 col-lg-5">
       <div className="single-sidebar fixed-sidebar">
         {/* Contact Form */}
-        <PropertyContactForm id={slug}/>
+        {/* <PropertyContactForm id={slug}/> */}
+           {/* WhatsApp Share Button */}
+<div className="widget-box single-property-contact d-flex flex-column align-items-center">
+  <a
+    href={`https://api.whatsapp.com/send?text=Check out this property: ${typeof window !== "undefined" ? window.location.href : ""}`}
+    target="_blank"
+    rel="noopener noreferrer"
+    className="btn btn-success rounded-circle d-flex justify-content-center align-items-center shadow"
+    style={{ 
+      bottom: "20px", 
+      right: "20px", 
+      width: "65px", 
+      height: "65px"
+    }}
+  >
+    <svg 
+      xmlns="http://www.w3.org/2000/svg" 
+      width="35" 
+      height="35" 
+      viewBox="0 0 32 32" 
+      fill="white"
+    >
+      <path d="M16 .4c-8.8 0-16 7.2-16 16 0 2.8.7 5.5 2 7.9l-2 7.7 7.9-2c2.3 1.2 5 1.9 7.8 1.9 8.8 0 16-7.2 16-16S24.8.4 16 .4zm0 29c-2.5 0-4.9-.7-7-2.1l-.5-.3-4.7 1.2 1.3-4.6-.3-.5c-1.3-2.1-2-4.6-2-7.1 0-7.4 6.1-13.5 13.5-13.5S29.5 8.6 29.5 16 23.4 29.4 16 29.4zm7.4-10.2c-.4-.2-2.3-1.1-2.7-1.2-.4-.2-.7-.2-1 .2-.3.4-1.1 1.2-1.3 1.4-.2.2-.5.2-1 .1s-2-.7-3.8-2.2c-1.4-1.2-2.3-2.7-2.5-3.1s0-.7.2-.9c.2-.2.4-.5.6-.7.2-.2.3-.4.5-.6.2-.2.3-.4.5-.6.2-.2.1-.5 0-.7-.1-.2-1-2.5-1.4-3.5-.4-1-1-1-1.3-1.1h-1c-.3 0-.7.1-1 .5-.3.4-1.3 1.3-1.3 3.1s1.3 3.6 1.5 3.9c.2.2 2.6 4 6.3 5.6.9.4 1.6.7 2.1.9.9.3 1.7.3 2.3.2.7-.1 2.3-.9 2.7-1.8.3-.9.3-1.6.2-1.8-.1-.2-.3-.3-.7-.5z"/>
+    </svg>
+  </a>
+  <p className="mt-2 text-center fw-semibold text-success">Share to WhatsApp</p>
+</div>
+
 
         {/* Why Choose Us */}
         <div className="widget-box single-property-whychoose">
